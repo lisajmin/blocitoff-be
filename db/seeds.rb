f2929fecf2e3fa05fd45cd97deb156114c797c54
Lisa Min <lisajmin@gmail.com>
2017-08-03 20:26:46 -0700

    Checkpoint 6: Seeding data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+#Create users
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
+#Create items
+50.times do
+  Item.create!(
+    name: Faker::Hipster.sentence,
+    user: users.sample
+  )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"

9ce0a00531d250f49a810bb862977ce8a5ced812
Lisa Min <lisajmin@gmail.com>
2017-08-09 22:48:41 -0700

    Checkpoint 8: Revise hover action to mouseenter and mouseleave

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,9 +1,11 @@
 
 $("document").ready(function() {
-  $(".glyphicon-unchecked").hover(function(){
+  $(".glyphicon-unchecked").on('mouseenter',function(){
       $(this).removeClass('glyphicon-unchecked');
       $(this).addClass('glyphicon-check');
-    }, function() {
+  });
+
+  $(".glyphicon-unchecked").on('mouseleave',function(){
       $(this).removeClass('glyphicon-check');
       $(this).addClass('glyphicon-unchecked');
   });

db6526cbdcc1d8777dbe7412feb711b6a8cb7bcb
Lisa Min <lisajmin@gmail.com>
2017-08-10 21:06:06 -0700

    Checkpoint 9: Delete expired task items

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-  $(document).on('mouseenter', '.glyphicon', function(){
+  $(document).on('mouseenter', '.task-complete', function(){
       $(this).removeClass('glyphicon-unchecked');
       $(this).addClass('glyphicon-check');
   });
-
-  $(document).on('mouseleave', '.glyphicon', function(){
+  $(document).on('mouseleave', '.task-complete', function(){
       $(this).removeClass('glyphicon-check');
       $(this).addClass('glyphicon-unchecked');
   });
